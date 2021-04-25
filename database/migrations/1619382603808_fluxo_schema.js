56e0599d39d9b3fca957911aa1ea82f0d637edc9
DanielFraga <daniel@danielf.com.br>
2021-04-25 17:39:54 -0300

    alteracoes nos fluxos e validacao de senhas

---
+++ database/migrations/1619382603808_fluxo_schema.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class FluxoSchema extends Schema {
+  up () {
+    this.create('fluxos', (table) => {
+      table.increments()
+      table.string('nomedoprocesso', 254)
+      table.string('fontedodado', 254)
+      table.
+      table.string('informacoescoletadas')
+      table.string('razao')
+      table.string('comoearmazenado')
+      table.string('protecao')
+      table.string('seguranca')
+      table.string('prazodeeliminacao')
+      table.string('justificativa')
+      table.string('menoresdeidade')
+      table.string('dadossensiveis')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('fluxos')
+  }
+}
+
+module.exports = FluxoSchema
