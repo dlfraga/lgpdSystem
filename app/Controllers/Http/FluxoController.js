e6576023699c7e277b43fd7363539f1c2196f20c
DanielFraga <daniel@danielf.com.br>
2021-05-05 21:30:47 -0300

    alterado fluxo controller

--- app/Controllers/Http/FluxoController.js
+++ app/Controllers/Http/FluxoController.js
@@ -59,31 +59,32 @@ class FluxoController {
             const fluxo = new Fluxo();
             fluxo.user_id = auth.user.id;
             console.log(auth.user.id);
-        }
-        fluxo.nomedoprocesso = request.input('nomedoprocesso');
-        fluxo.fontedodado = request.input('fontedodado');
-        fluxo.informacoescoletadas = request.input('informacoescoletadas');
-        fluxo.razao = request.input('razao');
-        fluxo.comoearmazenado = request.input('comoearmazenado');
-        fluxo.protecao = request.input('protecao');
-        fluxo.seguranca = request.input('seguranca');
-        fluxo.prazodeeliminacao = request.input('prazodeeliminacao');
-        fluxo.justificativa = request.input('justificativa');
-        if (request.input('menoresdeidade') == 'true') {
-            fluxo.menoresdeidade = 1;
-        } else {
-            fluxo.menoresdeidade = 0;
+            fluxo.nomedoprocesso = request.input('nomedoprocesso');
+            fluxo.fontedodado = request.input('fontedodado');
+            fluxo.informacoescoletadas = request.input('informacoescoletadas');
+            fluxo.razao = request.input('razao');
+            fluxo.comoearmazenado = request.input('comoearmazenado');
+            fluxo.protecao = request.input('protecao');
+            fluxo.seguranca = request.input('seguranca');
+            fluxo.prazodeeliminacao = request.input('prazodeeliminacao');
+            fluxo.justificativa = request.input('justificativa');
+            if (request.input('menoresdeidade') == 'true') {
+                fluxo.menoresdeidade = 1;
+            } else {
+                fluxo.menoresdeidade = 0;
+            }
+
+            if (request.input('dadossensiveis') == 'true') {
+                fluxo.dadossensiveis = 1;
+            } else {
+                fluxo.dadossensiveis = 0;
+            }
+
+            await fluxo.save()
+            session.flash({ success: 'Fluxo salvo com sucesso!' })
+            return response.redirect('/meusFluxos')
         }
 
-        if (request.input('dadossensiveis') == 'true') {
-            fluxo.dadossensiveis = 1;
-        } else {
-            fluxo.dadossensiveis = 0;
-        }
-
-        await fluxo.save()
-        session.flash({ success: 'Fluxo salvo com sucesso!' })
-        return response.redirect('/meusFluxos')
     }
 }
 
