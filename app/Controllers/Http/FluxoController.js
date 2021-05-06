9368cb234b5f418aca0a5e858ffacb69d12b3b21
DanielFraga <daniel@danielf.com.br>
2021-05-05 21:19:34 -0300

    adicionada edicao de fluxo

--- app/Controllers/Http/FluxoController.js
+++ app/Controllers/Http/FluxoController.js
@@ -5,16 +5,61 @@ const User = use('App/Models/User')
 const FonteDadosFluxo = use('App/Models/FonteDadosFluxo.js')
 
 class FluxoController {
-    async index({ view, auth }) {
-        const currentUser = await User.find(auth.user.id);               
-        const fluxos = await currentUser.fluxos().fetch();
-        return view.render('meusfluxos', {fluxos: fluxos.toJSON()})       
+    async index({ request, view }) {
+        const requestParams = request.params;
+        if (requestParams == null) {
+            console.log('todos fluxos');
+            const fluxos = await Fluxo.query().with('user').fetch();
+            return view.render('meusfluxos', { fluxos: fluxos.toJSON() })
+
+        } else if (requestParams.fluxo != null) {
+            const fluxoAEditar = request.params.fluxo;
+            const fluxoAtual = await Fluxo.find(fluxoAEditar);
+            return view.render('editarfluxo', { fluxoaeditar: fluxoAtual.toJSON() })
+
+        } else {
+            console.log('all fontes dados 2');
+            const fluxos = await Fluxo.query().with('user').fetch();
+            return view.render('meusfluxos', { fluxos: fluxos.toJSON() })
+        }
     }
 
     async store({ request, response, session, auth }) {
-        const fluxo = new Fluxo();
-        fluxo.user_id = auth.user.id;
-        console.log(auth.user.id);
+        const requestParams = request.params;
+        if (requestParams.fluxo != null) {
+            console.log('fluxoatual')
+            const fluxo = await await Fluxo.find(request.params.fluxo);
+            fluxo.nomedoprocesso = request.input('nomedoprocesso');
+            fluxo.fontedodado = request.input('fontedodado');
+            fluxo.informacoescoletadas = request.input('informacoescoletadas');
+            fluxo.razao = request.input('razao');
+            fluxo.comoearmazenado = request.input('comoearmazenado');
+            fluxo.protecao = request.input('protecao');
+            fluxo.seguranca = request.input('seguranca');
+            fluxo.prazodeeliminacao = request.input('prazodeeliminacao');
+            fluxo.justificativa = request.input('justificativa');
+            if (request.input('menoresdeidade') == 'true') {
+                fluxo.menoresdeidade = 1;
+            } else {
+                fluxo.menoresdeidade = 0;
+            }
+
+            if (request.input('dadossensiveis') == 'true') {
+                fluxo.dadossensiveis = 1;
+            } else {
+                fluxo.dadossensiveis = 0;
+            }
+
+            await fluxo.save()
+            session.flash({ success: 'Fluxo salvo com sucesso!' })
+            return response.redirect('/meusFluxos')
+
+        } else {
+            console.log('fluxonovo')
+            const fluxo = new Fluxo();
+            fluxo.user_id = auth.user.id;
+            console.log(auth.user.id);
+        }
         fluxo.nomedoprocesso = request.input('nomedoprocesso');
         fluxo.fontedodado = request.input('fontedodado');
         fluxo.informacoescoletadas = request.input('informacoescoletadas');
@@ -40,9 +85,6 @@ class FluxoController {
         session.flash({ success: 'Fluxo salvo com sucesso!' })
         return response.redirect('/meusFluxos')
     }
-
-    
-
 }
 
 module.exports = FluxoController
--- start/routes.js
+++ start/routes.js
@@ -19,9 +19,13 @@ const Route = use('Route')
 Route.on('/').render('index')
 Route.on('/esqueciMinhaSenha').render('esqueciMinhaSenha').middleware('guest');
 Route.on('/createNewAdmin').render('createnewadmin')
-Route.get('/dashboard', 'DashboardController.index').middleware('auth');
-Route.get('/meusFluxos', 'FluxoController.index').middleware('auth');
+Route.on('/editarFluxo').render('editarFluxo').middleware('auth')
 Route.on('/novoFluxo').render('novofluxo').middleware('auth');
+
+
+Route.get('/dashboard', 'DashboardController.index').middleware('auth');
+
+
 
 Route.get('users/:id', 'UserController.show').middleware('auth');
 
@@ -31,7 +35,8 @@ Route.post('/store', 'UserController.store').middleware('auth');
 Route.get('/logout', 'UserController.logout').middleware('auth');
 
 /**Resourcer=>  */
-Route.resource('fluxos', 'FluxoController').middleware('auth');
+Route.get('/meusFluxos', 'FluxoController.index').middleware('auth');
+Route.resource('fluxos/:fluxo?', 'FluxoController').middleware('auth');
 Route.resource('solicitacoes', 'SolicitacoesController');
 Route.resource('fonteDadosFluxo/:nome?', 'FontedadosfluxoController').middleware('auth');
 Route.resource('meuPerfil', 'PerfilController');
